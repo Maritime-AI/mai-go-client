af653b6950b3e54da710c62e00039d7ef3c9f9cd
Malcolm Monroe <malcolm@quantgene.com>
2024-11-18 19:35:01 -0800

    Updates to client

--- example/main.go
+++ example/main.go
@@ -4,17 +4,12 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/Maritime-AI/mai-go-client/aiapi"
-	"github.com/go-openapi/runtime"
-	"github.com/go-openapi/strfmt"
-
-	"github.com/go-openapi/runtime/client"
+	"github.com/Maritime-AI/mai-go-client/client"
 )
 
 func main() {
 	// NewDefaultClient returns a new default authenticator client
 	host := "mai.10ure.com"
-	basePath := "api/v1"
 
 	apiToken := os.Getenv("MAI_API_TOKEN")
 	if len(apiToken) == 0 {
@@ -31,27 +26,12 @@ func main() {
 		panic("ORG_REF_ID is not set")
 	}
 
-	transport := client.New(host, basePath, []string{"http"})
+	cli := client.NewClient(host, apiToken)
 
-	// Initialize the formats registry
-	formats := strfmt.Default
-
-	// Initialize the client
-	client := aiapi.New(transport, formats)
-
-	auth := runtime.ClientAuthInfoWriterFunc(func(req runtime.ClientRequest, _ strfmt.Registry) error {
-		return req.SetHeaderParam("Authorization", apiToken)
-	})
-
-	params := aiapi.NewGetPartnersOrganizationsIDConversationSessionIDParams().
-		WithSessionID(sessionID).
-		WithID(orgRefID)
-
-	// Use the client...
-	resp, err := client.GetPartnersOrganizationsIDConversationSessionID(params, auth)
+	resp, err := cli.GetConversation(sessionID, orgRefID)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("messages", resp.Payload.Messages)
+	fmt.Println("messages", resp)
 }
