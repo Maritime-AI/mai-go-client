7b9948227467a3e57aaa3be6648d8c08680ded9b
Malcolm Monroe <malcolm@quantgene.com>
2025-05-19 07:46:47 -0700

    Add post user chat and get user conversation client funcs

--- client.go
+++ client.go
@@ -158,6 +158,49 @@ func (c *Client) PostChatMessage(orgRefID string, sessionID *string,
 	return &respData, nil
 }
 
+// PostUserChatMessage sends a user chat message to the oceo maritime ai api
+func (c *Client) PostUserChatMessage(userID string, sessionID *string,
+	message string) (*models.ChatMessageResponse, error) {
+
+	url := fmt.Sprintf("%s://%s/api/v1/partners/users/%s/converse", c.protocol, c.host, userID)
+
+	data, err := json.Marshal(&models.ChatMessageParams{
+		Message:   message,
+		SessionID: sessionID,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal chat message params: %w", err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewReader(data))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create http request: %w", err)
+	}
+	req.Header.Set("Authorization", c.apiToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	httpCli := http.Client{
+		Timeout: c.timeout,
+	}
+
+	resp, err := httpCli.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send http request: %w", err)
+	}
+
+	bs, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var respData models.ChatMessageResponse
+	if err = json.Unmarshal(bs, &respData); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return &respData, nil
+}
+
 // GetConversation retrieves the conversation for a given session ID and organization reference ID
 func (c *Client) GetConversation(sessionID, orgRefID string) (*models.ChatMessageResponse, error) {
 	transport := client.New(c.host, basePath, []string{c.protocol})
@@ -178,6 +221,33 @@ func (c *Client) GetConversation(sessionID, orgRefID string) (*models.ChatMessag
 	WithTimeout(c.timeout)
 
 	resp, err := client.GetPartnersOrganizationsIDConversationSessionID(params, auth)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Payload, nil
+}
+
+// GetUserConversation retrieves the conversation for a given session ID and user reference ID
+func (c *Client) GetUserConversation(sessionID, userID string) (*models.ChatMessageResponse, error) {
+	transport := client.New(c.host, basePath, []string{c.protocol})
+
+	// Initialize the formats registry
+	formats := strfmt.Default
+
+	// Initialize the client
+	client := aiapi.New(transport, formats)
+
+	auth := runtime.ClientAuthInfoWriterFunc(func(req runtime.ClientRequest, _ strfmt.Registry) error {
+		return req.SetHeaderParam("Authorization", c.apiToken)
+	})
+
+	params := aiapi.NewGetPartnersUsersIDConversationSessionIDParams().
+		WithSessionID(sessionID).
+		WithID(userID)
+	WithTimeout(c.timeout)
+
+	resp, err := client.GetPartnersUsersIDConversationSessionID(params, auth)
 	if err != nil {
 		return nil, err
 	}
