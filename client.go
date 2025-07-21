bc1c22ea190be5520d693528003f97ec4beb7fad
Malcolm Monroe <malcolm@quantgene.com>
2025-07-21 13:14:08 -0500

    More updates to get user conversation

--- client.go
+++ client.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/client"
-	runtimeclient "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
 )
 
@@ -159,6 +158,33 @@ func (c *Client) PostChatMessage(orgRefID string, sessionID *string,
 	return &respData, nil
 }
 
+// GetConversation retrieves the conversation for a given session ID and organization reference ID
+func (c *Client) GetConversation(sessionID, orgRefID string) (*models.ChatMessageResponse, error) {
+	transport := client.New(c.host, basePath, []string{c.protocol})
+
+	// Initialize the formats registry
+	formats := strfmt.Default
+
+	// Initialize the client
+	client := aiapi.New(transport, formats)
+
+	auth := runtime.ClientAuthInfoWriterFunc(func(req runtime.ClientRequest, _ strfmt.Registry) error {
+		return req.SetHeaderParam("Authorization", c.apiToken)
+	})
+
+	params := aiapi.NewGetPartnersOrganizationsIDConversationSessionIDParams().
+		WithSessionID(sessionID).
+		WithID(orgRefID)
+	WithTimeout(c.timeout)
+
+	resp, err := client.GetPartnersOrganizationsIDConversationSessionID(params, auth)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Payload, nil
+}
+
 // PostUserChatMessage sends a user chat message to the oceo maritime ai api
 func (c *Client) PostUserChatMessage(userID string, sessionID *string,
 	message string) (*models.ChatMessageResponse, error) {
@@ -202,57 +228,34 @@ func (c *Client) PostUserChatMessage(userID string, sessionID *string,
 	return &respData, nil
 }
 
-// GetConversation retrieves the conversation for a given session ID and organization reference ID
-func (c *Client) GetConversation(sessionID, orgRefID string) (*models.ChatMessageResponse, error) {
-	transport := client.New(c.host, basePath, []string{c.protocol})
-
-	// Initialize the formats registry
-	formats := strfmt.Default
-
-	// Initialize the client
-	client := aiapi.New(transport, formats)
-
-	auth := runtime.ClientAuthInfoWriterFunc(func(req runtime.ClientRequest, _ strfmt.Registry) error {
-		return req.SetHeaderParam("Authorization", c.apiToken)
-	})
-
-	params := aiapi.NewGetPartnersOrganizationsIDConversationSessionIDParams().
-		WithSessionID(sessionID).
-		WithID(orgRefID)
-	WithTimeout(c.timeout)
-
-	resp, err := client.GetPartnersOrganizationsIDConversationSessionID(params, auth)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.Payload, nil
-}
-
 // GetUserConversation retrieves the conversation for a given session ID and user reference ID
 func (c *Client) GetUserConversation(sessionID, userID string) (*models.ChatMessageResponse, error) {
-	transport := client.New(c.host, basePath, []string{c.protocol})
-
-	// Initialize the formats registry
-	formats := strfmt.Default
-
-	// Initialize the client
-	client := aiapi.New(transport, formats)
-
-	//auth := runtime.ClientAuthInfoWriterFunc(func(req runtime.ClientRequest, _ strfmt.Registry) error {
-	//	return req.SetHeaderParam("Authorization", c.apiToken)
-	//})
-	authInfo := runtimeclient.APIKeyAuth("Authorization", "header", c.apiToken)
-
-	params := aiapi.NewGetPartnersUsersIDConversationSessionIDParams().
-		WithSessionID(sessionID).
-		WithID(userID)
-	WithTimeout(c.timeout)
-
-	resp, err := client.GetPartnersUsersIDConversationSessionID(params, authInfo)
+	url := fmt.Sprintf("%s://%s/api/v1/partners/users/%s/conversation/%s", c.protocol, c.host, userID, sessionID)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create http request: %w", err)
+	}
+	req.Header.Set("Authorization", c.apiToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	httpCli := http.Client{
+		Timeout: c.timeout,
 	}
 
-	return resp.Payload, nil
+	resp, err := httpCli.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send http request: %w", err)
+	}
+
+	bs, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var respData models.ChatMessageResponse
+	if err = json.Unmarshal(bs, &respData); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return &respData, nil
 }
